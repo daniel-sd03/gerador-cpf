fe4adf5ef86a7b638c29fb0101ad69b10311b198
daniel sodre <danielsodre2024@gmail.com>
2025-04-15 07:26:04 -0300

    feat: criar entidade Usuario com login e senha

---
+++ src/main/java/br/com/sodresoftwares/geradorcpf/model/Usuario.java
@@ -0,0 +1,40 @@
+package br.com.sodresoftwares.geradorcpf.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String login;
+	private String senha;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+}
