b5c81e1d182f5c1a36274e3eb9cd5dda4afba3c5
daniel sodre <danielsodre2024@gmail.com>
2025-04-16 20:33:50 -0300

    feat: configurar login, logout e controle de acesso com Spring Security

---
+++ src/main/java/br/com/sodresoftwares/geradorcpf/security/WebConfigSecurity.java
@@ -0,0 +1,34 @@
+package br.com.sodresoftwares.geradorcpf.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class WebConfigSecurity {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(auth -> auth
+                    .anyRequest().permitAll()//perminto acesso a todas as paginas
+            )
+            .formLogin(login -> login //pagina de login padrão do spring security
+            		.permitAll()
+                .defaultSuccessUrl("/", true)
+                .failureUrl("/login?erro=true")
+            )
+            .logout(logout -> logout
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/") // Redireciona para a home após logout
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+            );
+        return http.build();
+    }
+    
+
+}
